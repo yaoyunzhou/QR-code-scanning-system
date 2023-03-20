9afd6c41feb77335f93aa1b9a46dacca1890d275
小飞鸟 <63948417+small-cai@users.noreply.github.com>
2023-03-20 10:39:12 +0000

    inst

---
+++ QR/test.py
@@ -0,0 +1,17 @@
+import qrcode
+
+# 创建QRCode对象
+qr = qrcode.QRCode(version=1, box_size=10, border=2)
+
+# 添加数据到QRCode
+data = "https://github.com/small-cai/QR-code-scanning-system"
+qr.add_data(data)
+
+# 编码数据
+qr.make(fit=True)
+
+# 创建图像
+img = qr.make_image(fill_color="black", back_color="white")
+
+# 显示图像
+img.save("test.png")
